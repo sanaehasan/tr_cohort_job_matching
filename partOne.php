17fe03e5e2e3ff95eaa8e18706fb5c208310c7e5
sanae <sanae.k1986@gmail.com>
2020-08-18 15:03:52 +0100

    updates to the code
    updates

--- partOne.php
+++ partOne.php
@@ -1,13 +1,27 @@
 <?php
 
-
+class job extends CI_Controller{
 //we suppose to get an input type form and get the skills and chreteria , i put them as vriables in the example
-        $skills=array("wants to work remote", "react experience" ,"looking for frontend");
-         $chreteria = array( "remote", "flexible working hours") ;
         
          //put all the text together and compaire the two texts
+         public function jobmatch($skills,$chreteria){
          $s= implode(",", $skills);
          $c= implode(",", $chreteria);
         //show the similarity penrcentages using php function 
          $sim =similar_text($s, $c,$perc);
-         var_dump("similarity: $sim ($perc %)\n");
+         return $perc;
+         }
+         
+         public function output(){
+             $skills=array("wants to work remote", "react experience" ,"looking for frontend");
+             $chreteria = array( "remote", "flexible working hours") ;
+             $job= new job();
+             $percentage = $job->jobmatch($skills,$chreteria);
+             
+             if ((int) $percentagec>50){
+                echo "job match";
+                }else{
+                 echo "no match";
+           }
+         }
+}
